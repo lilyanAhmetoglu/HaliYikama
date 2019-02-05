69a5e685515b4ad3097d30c1aaf8d8936aa233e1
beyan ahmad <adolf-sy@hotmail.com>
2019-02-05 15:49:22 +0300

    Proje dosyası ekle.

---
+++ Haliyikama/Data/Models/Labor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// this model is for the service that we need to add to the site
+namespace Haliyikama.Data.Models
+{
+	public class Labor
+	{
+		public int LaborId { get; set; }
+		public string Name { get; set; }
+		public string ShortDescription { get; set; }
+		public string LongDescription { get; set; }
+		public int Price { get; set; }
+		public int NewPrice { get; set; }
+		public string ImageUrl { get; set; }
+		public bool IsDiscounted { get; set; }
+	}
+}
