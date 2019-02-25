cc39eff2722d0519974a95ca22c48b5415a0b4fa
beyan ahmad <adolf-sy@hotmail.com>
2019-02-25 15:45:12 +0300

    control panel css and js and html addign

--- HaliYikamaWebAPI/Startup.cs
+++ HaliYikamaWebAPI/Startup.cs
@@ -56,16 +56,18 @@ namespace HaliYikamaWebAPI
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
-			if (env.IsDevelopment())
+			/*if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
 			else
 			{
 				app.UseHsts();
-			}
+			}*/
+			app.UseDeveloperExceptionPage();
 			app.UseMvc();
-			app.UseDefaultFiles();
+			//app.UseDefaultFiles();
+			app.UseStatusCodePages();
 			app.UseStaticFiles();
 		}
 	}
