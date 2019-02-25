3ecc80e8c72f70284c27c77a8882b354b5255319
beyan ahmad <adolf-sy@hotmail.com>
2019-02-25 17:25:21 +0300

    Control Panel Hizmetler alt Hizmetler tasarimi

---
+++ HaliYikamaWebAPI/Controllers/HizmetlerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaliYikamaWebAPI.Controllers
+{
+    public class HizmetlerController : Controller
+    {
+		[Route("hizmetler")]
+		public IActionResult Index()
+        {
+            return View();
+        }
+
+		[Route("hizmetler/alt-hizmetler")]
+		public ViewResult AltHizmetler()
+		{
+			
+			return View();
+		}
+	}
+}
